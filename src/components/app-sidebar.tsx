580a4b31a1c8c65e81dcf163df1a9884fa1407ae
Al Fatih Muzakki <malfazakki@gmail.com>
2025-05-30 08:41:25 +0700

    feat(app): setting up app layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Newspaper, Tag, LogOut } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+	{
+		title: "Articles",
+		path: "/admin/articles",
+		icon: Newspaper,
+	},
+	{
+		title: "Categories",
+		path: "/admin/categories",
+		icon: Tag,
+	},
+	{
+		title: "Logout",
+		path: "/admin/logout",
+		icon: LogOut,
+	},
+];
+
+export function AppSidebar() {
+	const pathname = usePathname();
+	return (
+		<Sidebar>
+			<SidebarContent className='bg-primary '>
+				<SidebarGroup className='py-6 px-4'>
+					<SidebarGroupLabel>
+						<Image src='/assets/image/logo_white.svg' alt='Logo White' width={134} height={24} />
+					</SidebarGroupLabel>
+					<SidebarGroupContent className='mt-6'>
+						<SidebarMenu>
+							{items.map((item) => {
+								return (
+									<SidebarMenuItem key={item.title}>
+										<SidebarMenuButton
+											asChild
+											isActive={pathname === item?.path || pathname.startsWith(item?.path)}
+											className='data-[active=true]:bg-sidebar-primary data-[active=true]:text-white text-white hover:bg-sidebar-primary hover:text-white font-medium py-5'
+											tooltip={item.title}
+											size='default'
+										>
+											<Link href={item.path}>
+												<item.icon />
+												<span>{item.title}</span>
+											</Link>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								);
+							})}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
+}
