ec13f4b966e8a96bf1206dd222829c7b4e2275f1
Al Fatih Muzakki <malfazakki@gmail.com>
2025-05-31 17:10:43 +0700

    feat(layout): implement user layout with navbar and footer components, enhance login handling in user layout, and update article list with loading skeletons

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -3,16 +3,25 @@ import { AuthState } from "@/types/authTypes";
 
 export const useAuthStore = create<AuthState>((set) => ({
 	token: typeof window !== "undefined" ? localStorage.getItem("token") : null,
-	role: typeof window !== "undefined" && localStorage.getItem("role") ? localStorage.getItem("role") : null,
-	isAuthenticated: typeof window !== "undefined" ? !!localStorage.getItem("token") : false,
+	role: typeof window !== "undefined" ? localStorage.getItem("role") : null,
+	isAuthenticated:
+		typeof window !== "undefined" ? !!localStorage.getItem("token") && !!localStorage.getItem("role") : false,
 	setAuth: (token, role) => {
 		localStorage.setItem("token", token);
-		localStorage.setItem("user", role);
-		set({ token, role });
+		localStorage.setItem("role", role);
+		set({
+			token,
+			role,
+			isAuthenticated: !!token && !!role,
+		});
 	},
 	clearAuth: () => {
 		localStorage.removeItem("token");
-		localStorage.removeItem("user");
-		set({ token: null, role: null });
+		localStorage.removeItem("role");
+		set({
+			token: null,
+			role: null,
+			isAuthenticated: false,
+		});
 	},
 }));
